90dd824020ce82860116bebc460890bd8c914a8e
bdchris1 <bdchris1@ptd.net>
2015-04-26 18:05:47 -0400

    Commit for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions demonstrate the concept of lexical scoping of variables.
+## This is useful when storing the result of one function in a variable to be used
+## in other functions without having to duplicate processing steps.
 
-## Write a short comment describing this function
+##This function generates a matrix and uses the <<- operator to make the matrix available 
+## outside the scope of the function in which it was declared (e.g., made available to the 'cacheSolve' function).
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
+## The following function tests for the presence of a cached matrix.
+## If a cached matrix exists (and it is invertible), the solve() function is called to create the inverse
 
-## Write a short comment describing this function
+  cacheSolve <- function(x, ...) {
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+      message("getting cached data.")
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
+  }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Code can be tested from the console as follows:
+
+## 1) call: x <- makeCacheMatrix(matrix(sample.int(10, 10 * 10, TRUE), 10, 10)); generates a 10 x 10 matrix of random values between 1 and 10
+## 2) call: cacheSolve(x)
+
